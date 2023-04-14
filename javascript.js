c4fcedb0e701073e4ebca1a112eca4156291f648
Guy Foster <125807123+guyfoz77@users.noreply.github.com>
2023-04-14 09:06:01 +0100

    Write function to create a row of pixels

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 //run createPixel() 16 times to fill the div with pixels.
 
 let mainContainer = document.querySelector('.mainContainer');
+let resolution = 16;
 
 function createPixel() {
     let pixel = document.createElement('div');
@@ -8,5 +9,15 @@ function createPixel() {
     return pixel;
 }
 
+function createRow() {
+    let row = document.createElement('div')
+    row.classList.add('row');
+    for (let i = resolution; i >= 1; i--) {
+        row.appendChild(createPixel());
+    }
+    return row;
+}
 
-mainContainer.appendChild(createPixel());
+mainContainer.appendChild(createRow());
+
+// mainContainer.appendChild(createPixel());
