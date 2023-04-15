c6ba3bd8aa84c73a470e8c111de4eed511f8d11c
guyfoz77 <125807123+guyfoz77@users.noreply.github.com>
2023-04-15 15:28:19 +0100

    Make resolution buttons work

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ let resolution = 50;
 let opacityChange = 0.3;
 let containerSize = 700;
 const sizeButtons = document.querySelectorAll('.sizeButton');
+const resolutionButtons = document.querySelectorAll('.resolutionButton');
 
 function createPixel() {
     let pixel = document.createElement('div');
@@ -10,27 +11,36 @@ function createPixel() {
     return pixel;
 }
 
-function createRow() {
+function createRow(res) {
     let row = document.createElement('div')
     row.classList.add('row');
-    for (let i = resolution; i >= 1; i--) {
+    for (let i = res; i >= 1; i--) {
         row.appendChild(createPixel());
     }
     return row;
 }
 
-//adds rows to the main container.
-for (let i = resolution; i >= 1; i--) {
-    mainContainer.appendChild(createRow());
+function initialise(res) {  //res is the intended number of pixels high and wide (set by global variable resolution initially)
+    for (let i = res; i >= 1; i--) {    
+        mainContainer.appendChild(createRow(res));  //adds rows to the main container.
+    }
+    const pixels = document.querySelectorAll('.pixel');
+
+    pixels.forEach(pixel => {       //adds event listeners to all the pixels to change opacity on mouseover.
+        pixel.style.opacity = 1;
+        pixel.addEventListener('mouseover', function () {
+            this.style.opacity -= opacityChange;
+        })
+})
 }
 
+initialise(resolution)
 
-//adds event listeners to all the pixels to change opacity on mouseover.
-const pixels = document.querySelectorAll('.pixel');
-pixels.forEach(pixel => {
-    pixel.style.opacity = 1;
-    pixel.addEventListener('mouseover', function () {
-        this.style.opacity -= opacityChange;
+resolutionButtons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        mainContainer.textContent = '';     //this line removes all th old pixels and replaces them with a blank string.
+        resolution = e.target.dataset.resolution;
+        initialise(resolution);
     })
 })
 
@@ -39,6 +49,5 @@ sizeButtons.forEach(button => {
         containerSize = e.target.dataset.size;
         mainContainer.style.width = containerSize;
         mainContainer.style.height = containerSize;
-        pixels.forEach(pixel => pixel.style.opacity = 1);   //resets all pixels opacity - do we need to do this for just a resize?
     })
 })
